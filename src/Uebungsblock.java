26f479dc6a38bb167cfa2064c0ff67926f22f072
14214 <14214@A001-02.nordakademie.intern>
2015-11-10 11:46:02 +0100

    übungen 1-5

--- src/Uebungsblock.java
+++ src/Uebungsblock.java
@@ -19,8 +19,9 @@ public class Uebungsblock {
 			Zeichenfenster.setzeFarbeSchwarz();
 
 			if (nr == 1) {
-				uebung1();
+				uebung1();	
 			}
+			
 			if (nr == 2) {
 				uebung2();
 			}
@@ -48,23 +49,84 @@ public class Uebungsblock {
 
 	public static void uebung1() {
 		// TODO Implementieren Sie die Uebung 1
+		
+		//Horizontale
+		Zeichenfenster.zeichneLinie(200, 200, 600, 200);
+		Zeichenfenster.zeichneLinie(200, 500, 600, 500);
+		//Vertikale
+		Zeichenfenster.zeichneLinie(600, 200, 600, 500);
+		Zeichenfenster.zeichneLinie(200, 200, 200, 500);
+		
+		
+		Zeichenfenster.zeichnePunkt(400, 400);
+		Zeichenfenster.zeichnePunkt(350, 300);
+		Zeichenfenster.zeichnePunkt(450, 300);
+		
+		Zeichenfenster.zeichneEllipse(400, 350, 400, 300);
 	}
 
 	public static void uebung2() {
 		// TODO Implementieren Sie die Uebung 2
+		
+		int t = 0;
+		while ( t <= 800 ) {
+			Zeichenfenster.zeichneLinie(0, 800-t, 800-t, 0);
+		    t = t + 10;
+		}
+
 	}
 
 	public static void uebung3() {
 		// TODO Implementieren Sie die Uebung 3
+		
+		int d = 0;
+		while ( d <= 300 ) {
+			Zeichenfenster.zeichneEllipse(200+d, 400, 100+d, 100+d);
+		    d = d + 5;
+		}
 	}
 
 	public static void uebung4() {
 		// TODO Implementieren Sie die Uebung 4
+		int c = 0;
+		while ( c <= 600 ) {
+			Zeichenfenster.zeichneEllipse(400, 400, 800-c, 200+c);
+		    c = c + 10;
+		}
 	}
 
 	public static void uebung5() {
 		// TODO Implementieren Sie die Uebung 5
+		int x = 0;
+		while ( x <= 3200 ) {
+			Zeichenfenster.zeichneLinie(0+x, 0, 800, 0+x);
+			Zeichenfenster.zeichneLinie(0, 0+x, 0+x, 800);
+			
+			Zeichenfenster.zeichneLinie(0+x, 800, 800, 800-x);
+			Zeichenfenster.zeichneLinie(0, 800-x, 0+x, 0);
+			
+		    x = x + 8;
+		  
+		  /*  int x1 = 0;
+			while ( x1 <= 3200 ) {
+				Zeichenfenster.zeichneLinie(0+x1, 0, 800, 800+x1);
+			    x1 = x1 + 8;    
+			   
+			    int x11 = 0;
+				while ( x11 <= 3200 ) {
+					Zeichenfenster.zeichneLinie(800+x11, 0, 800, 0+x11);
+				    x11 = x11 + 8;
+				    
+				    int x111 = 0;
+					while ( x111 <= 3200 ) {
+						Zeichenfenster.zeichneLinie(800+x111, 0, 800, 800+x111);
+					    x111 = x111 + 8;
+					}
+				}
+			}*/
+		}		
 	}
+
 
 	public static void uebung6() {
 		// TODO Implementieren Sie die Uebung 6
